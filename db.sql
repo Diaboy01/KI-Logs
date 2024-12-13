b960fa1353b2a4f1611a11fec451a04f9dd84536
Diaboy01 <marvin.prigenitz@gmx.de>
2024-12-13 10:38:49 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -14,3 +14,37 @@ CREATE TABLE IF NOT EXISTS paths (
     id INT AUTO_INCREMENT PRIMARY KEY,
     entry TEXT NOT NULL UNIQUE
 );
+
+-- Tabelle für Access-Logs
+CREATE TABLE IF NOT EXISTS access_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ip VARCHAR(255),
+    timestamp DATETIME,
+    request VARCHAR(255),
+    status_code INT,
+    user_agent TEXT,
+    referrer TEXT NULL
+);
+
+-- Tabelle für Error-Logs
+CREATE TABLE IF NOT EXISTS error_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
+    module VARCHAR(255),
+    severity VARCHAR(50),
+    pid INT,
+    client VARCHAR(255),
+    message TEXT
+);
+
+-- Tabelle für MyFiles-Access-Logs
+CREATE TABLE IF NOT EXISTS myfiles_access_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ip VARCHAR(255),
+    user VARCHAR(255),
+    timestamp DATETIME,
+    request VARCHAR(255),
+    status_code INT,
+    user_agent TEXT,
+    referrer TEXT NULL
+);
